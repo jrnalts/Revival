bba1ebe622ed9758291d655f1590a5b987e71623
jrnalts <jrnalts@gmail.com>
2020-11-04 14:13:48 +0800

    建立 user 與 voucher 之間關聯 設計 voucher 的 MVC 架構

---
+++ app/controllers/voucher_controller.rb
@@ -0,0 +1,29 @@
+class VoucherController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @vouchers = current_user.vouchers
+  end
+
+  def new
+    @voucher = current_user.vouchers.new
+  end
+
+  def create
+    @voucher = current_user.vouchers.new(voucher_params)
+    created_voucher = Voucher.find_by(tax_id: @voucher.tax_id)
+    if created_voucher
+      flash.now[:notice] = "您已於#{created_voucher.created_at}兌換過"
+      render :new
+    elsif @voucher.save
+      redirect_to vouchers_path, notice: "已成功兌換，您的兌換券序號為#{@voucher.serial}"
+    else
+      render :new
+    end 
+  end
+
+  private
+  def voucher_params
+    params.require(:voucher).permit(:name, :tax_id, :tel, :user_id, :serial)
+  end
+end
---
+++ spec/factories/vouchers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :voucher do
+    name { "MyString" }
+    uniform { 1 }
+    tel { 1 }
+    user { nil }
+    serial { 1 }
+  end
+end
